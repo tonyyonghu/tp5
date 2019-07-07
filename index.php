9256be4e56cd8f5b29277c2f94f4fe7a559a4135
tonyyonghu <1154771388@qq.com>
2019-07-07 15:18:15 +0800

    修改index.php 文件

--- index.php
+++ index.php
@@ -11,6 +11,10 @@ require str_replace("\\","/",__DIR__).'/thinkphp/base.php';
 // 执行应用
 //\think\App::run()->send();
 // 加载框架引导文件
-// 
+//
+echo 11111;
+echo 22222;
+echo 33333;
+echo 44444;
 require __DIR__ . '/thinkphp/start.php';
 ?>
